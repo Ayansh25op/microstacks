8297075eedcced3a4151e785c630a77ef7792937
Ayansh25op <87590978+Ayansh25op@users.noreply.github.com>
2022-09-24 16:15:37 +0530

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,25 @@
 input.onPinPressed(TouchPin.P0, function () {
-    keyboard.sendString(" ")
+    if (Status == 1) {
+        keyboard.sendString(" ")
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    Status = 1
+    basic.showIcon(IconNames.Yes)
 })
 input.onPinPressed(TouchPin.P2, function () {
-    keyboard.sendString(" ")
+    if (Status == 1) {
+        keyboard.sendString(" ")
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    Status = 2
+    basic.showIcon(IconNames.No)
 })
 input.onPinPressed(TouchPin.P1, function () {
-    keyboard.sendString(" ")
+    if (Status == 1) {
+        keyboard.sendString(" ")
+    }
 })
+let Status = 0
 keyboard.startKeyboardService()
-basic.forever(function () {
-	
-})
